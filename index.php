5d85cf147dec0bf745b12653f246c81ac69a9a57
Dhanush KT <dhanushkt787@gmail.com>
2018-03-31 18:51:32 +0530

    appointment form email validation fix

--- index.php
+++ index.php
@@ -590,7 +590,7 @@ if(isset($_POST['apointsubmit']))
                         <div class="col-md-6">
                             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label form-input-icon">
                                 <i class="fa fa-envelope-o"></i>
-                                <input name="apointemail" class="mdl-textfield__input" type="text" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" id="appointment-email">
+                                <input name="apointemail" class="mdl-textfield__input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" id="appointment-email">
                                 <label class="mdl-textfield__label" for="appointment-email">Email</label>
                                 <span class="mdl-textfield__error">Please Enter Valid Email!</span>
                             </div>
